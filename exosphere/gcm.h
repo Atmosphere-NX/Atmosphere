81b874cc14b7b12cd96272813120f647d855c07c
TuxSH <tuxsh@sfr.fr>
2018-02-20 19:29:44 +0100

    Fix minor build issues in gcm.c, etc.

--- exosphere/gcm.h
+++ exosphere/gcm.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-int gcm_decrypt_key(unsigned int keyslot, void *dst, size_t dst_size, const void *src, size_t src_size, const void *sealed_kek, size_t kek_size, const void *wrapped_key, size_t key_size, unsigned int usecase, int is_personalized);
+size_t gcm_decrypt_key(unsigned int keyslot, void *dst, size_t dst_size, const void *src, size_t src_size, const void *sealed_kek, size_t kek_size, const void *wrapped_key, size_t key_size, unsigned int usecase, int is_personalized);
 
 #endif
