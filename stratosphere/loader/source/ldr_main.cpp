79aba08295d3b96f704e085a1f0a4de886fab22d
Michael Scire <SciresM@gmail.com>
2018-04-17 17:26:28 -0600

    Stratosphere: Make dir for loader

---
+++ stratosphere/loader/source/ldr_main.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <switch.h>
+
+extern u32 __start__;
+
+u32 __nx_applet_type = AppletType_None;
+
+#define INNER_HEAP_SIZE 0x200000
+size_t nx_inner_heap_size = INNER_HEAP_SIZE;
+char   nx_inner_heap[INNER_HEAP_SIZE];
+
+void __libnx_initheap(void)
+{
+	void*  addr = nx_inner_heap;
+	size_t size = nx_inner_heap_size;
+
+	/* Newlib */
+	extern char* fake_heap_start;
+	extern char* fake_heap_end;
+
+	fake_heap_start = (char*)addr;
+	fake_heap_end   = (char*)addr + size;
+}
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
+
