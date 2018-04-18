033cd8df2439d08a78a9a8b5a09e910869254941
Michael Scire <SciresM@gmail.com>
2018-04-17 18:04:41 -0600

    Stratosphere: Launch Queue C style lib -> namespace

--- stratosphere/loader/source/ldr_launch_queue.hpp
+++ stratosphere/loader/source/ldr_launch_queue.hpp
@@ -6,14 +6,16 @@
 
 #define LAUNCH_QUEUE_ARG_SIZE_MAX (0x8000)
 
-typedef struct launch_item_t {
-    u64 tid;
-    u64 arg_size;
-    char args[LAUNCH_QUEUE_ARG_SIZE_MAX];
-} launch_item_t;
+namespace LaunchQueue { 
+    struct LaunchItem {
+        u64 tid;
+        u64 arg_size;
+        char args[LAUNCH_QUEUE_ARG_SIZE_MAX];
+    };
 
-Result launch_queue_add(launch_item_t item);
-int launch_queue_get_index(u64 TID);
-int launch_queue_get_free_index(u64 TID = 0);
-bool launch_queue_contains(u64 TID);
-void launch_queue_clear();
+    Result add(LaunchItem *item);
+    int get_index(u64 tid);
+    int get_free_index(u64 tid);
+    bool contains(u64 tid);
+    void clear();
+}
