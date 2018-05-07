7695b5bdbd94774d5a1055c26d4b226592d1412b
TuxSH <tuxsh@sfr.fr>
2018-05-07 10:53:41 +0200

    [stage2] Fix the ordering of rules in linker script

--- fusee/fusee-secondary/src/chainloader.c
+++ fusee/fusee-secondary/src/chainloader.c
@@ -1,6 +1,6 @@
 #include "chainloader.h"
 
-uint8_t g_payload_arg_data[PAYLOAD_ARG_DATA_MAX_SIZE] = {1};
+uint8_t __attribute__((used)) g_payload_arg_data[PAYLOAD_ARG_DATA_MAX_SIZE] = {0};
 
 #pragma GCC optimize (3)
 void relocate_and_chainload_main(uintptr_t load_address, uintptr_t src_address, size_t size, int argc) {
