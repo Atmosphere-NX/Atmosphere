633c5e95ed5cd0e3bc3df68919ff11e54d55673f
Kate J. Temkin <k@ktemkin.com>
2018-05-18 21:23:53 -0600

    fusee: correct constants in CAR

--- fusee/fusee-primary/src/car.h
+++ fusee/fusee-primary/src/car.h
@@ -49,8 +49,10 @@ enum {
  */
 enum {
     CLK_SOURCE_MASK        = (0b111 << 29),
-    CLK_SOURCE_FIRST       = 0,
-    CLK_DIVIDER_UNITY      = 0,
+    CLK_SOURCE_FIRST       = (0b000 << 29),
+
+    CLK_DIVIDER_MASK       = (0xff << 0),
+    CLK_DIVIDER_UNITY      = (0x00 << 0),
 };
 
 
@@ -65,8 +67,8 @@ enum {
 
 
 enum {
-    CLK_SOURCE_SDMMC1         = 19,
-    CLK_SOURCE_SDMMC4         = 21,   /* 0x54 into the the main source block */
+    CLK_SOURCE_SDMMC1         = 20,
+    CLK_SOURCE_SDMMC4         = 25,   /* 0x54 into the the main source block */
 
     CLK_SOURCE_SDMMC_LEGACY  = 0,   /* first in block Y */
 };
