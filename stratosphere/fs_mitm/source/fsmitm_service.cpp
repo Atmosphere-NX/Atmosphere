548903e3fbc4985fac6cbf6f707b17a1bf9353c1
Michael Scire <SciresM@gmail.com>
2018-06-10 01:19:21 -0600

    fs.mitm: Remove debugging test, fix postprocessing.

--- stratosphere/fs_mitm/source/fsmitm_service.cpp
+++ stratosphere/fs_mitm/source/fsmitm_service.cpp
@@ -3,17 +3,17 @@
 
 Result FsMitMService::dispatch(IpcParsedCommand &r, IpcCommand &out_c, u64 cmd_id, u8 *pointer_buffer, size_t pointer_buffer_size) {
     Result rc = 0xF601;
-    while (cmd_id == 18) {
-        
-    }
     return rc;
 }
 
 Result FsMitMService::postprocess(IpcParsedCommand &r, IpcCommand &out_c, u64 cmd_id, u8 *pointer_buffer, size_t pointer_buffer_size) {
-    Result rc = 0xF601;
-    while (cmd_id == 18) {
-        
-    }
+    struct {
+        u64 magic;
+        u64 result;
+    } *resp = (decltype(resp))r.Raw;
+    
+    Result rc = (Result)resp->result;
+    /* TODO: Hook here, if needed. */
     return rc;
 }
 
