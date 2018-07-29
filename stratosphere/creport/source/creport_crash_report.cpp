c70420d99616f1cf58210978946044be27d52c01
Michael Scire <SciresM@gmail.com>
2018-07-29 16:54:15 -0700

    creport: Actually change report paths.

--- stratosphere/creport/source/creport_crash_report.cpp
+++ stratosphere/creport/source/creport_crash_report.cpp
@@ -226,7 +226,7 @@ void CrashReport::SaveReport() {
     }
     
     /* Open report file. */
-    snprintf(report_path, sizeof(report_path) - 1, "sdmc:/atmosphere/crash reports/%011lu_%016lx.log", timestamp, process_info.title_id);
+    snprintf(report_path, sizeof(report_path) - 1, "sdmc:/atmosphere/crash_reports/%011lu_%016lx.log", timestamp, process_info.title_id);
     FILE *f_report = fopen(report_path, "w");
     if (f_report == NULL) {
         return;
@@ -235,7 +235,7 @@ void CrashReport::SaveReport() {
     fclose(f_report);
     
     /* Dump threads. */
-    snprintf(report_path, sizeof(report_path) - 1, "sdmc:/atmosphere/crash reports/dumps/%011lu_%016lx_thread_info.bin", timestamp, process_info.title_id);
+    snprintf(report_path, sizeof(report_path) - 1, "sdmc:/atmosphere/crash_reports/dumps/%011lu_%016lx_thread_info.bin", timestamp, process_info.title_id);
     f_report = fopen(report_path, "wb");
     this->thread_list.DumpBinary(f_report, this->crashed_thread_info.GetId());
     fclose(f_report);
