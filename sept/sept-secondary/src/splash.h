656053582acd18e301494e476a47ee4dba91900c
Michael Scire <SciresM@gmail.com>
2019-02-20 15:18:39 -0800

    Fix copy/pasted copyright notice

--- sept/sept-secondary/src/splash.h
+++ sept/sept-secondary/src/splash.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 naehrwert
+ * Copyright (c) 2018 CtCaer
  * Copyright (c) 2018 Atmosphère-NX
  *
  * This program is free software; you can redistribute it and/or modify it
