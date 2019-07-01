9a23fb4c74883d89103e127060e0faac333ba496
Connor Kurtz <connorkurtz22@gmail.com>
2019-07-01 03:40:12 -0700

    Fixed formatting in spl

--- stratosphere/spl/source/spl_general_service.hpp
+++ stratosphere/spl/source/spl_general_service.hpp
@@ -25,47 +25,47 @@ namespace sts::spl {
         protected:
             enum class CommandId {
                 /* 1.0.0+ */
-                GetConfig                   = 0,
-                ExpMod                      = 1,
-                GenerateAesKek              = 2,
-                LoadAesKey                  = 3,
-                GenerateAesKey              = 4,
-                SetConfig                   = 5,
-                GenerateRandomBytes         = 7,
-                ImportLotusKey              = 9,
-                DecryptLotusMessage         = 10,
-                IsDevelopment               = 11,
-                GenerateSpecificAesKey      = 12,
-                DecryptRsaPrivateKey        = 13,
-                DecryptRsaPrivateKeyDeprecated        = 13,
-                DecryptAesKey               = 14,
-                CryptAesCtr                 = 15,
-                ComputeCmac                 = 16,
-                ImportEsKey                 = 17,
-                UnwrapTitleKey              = 18,
-                LoadTitleKey                = 19,
+                GetConfig                      = 0,
+                ExpMod                         = 1,
+                GenerateAesKek                 = 2,
+                LoadAesKey                     = 3,
+                GenerateAesKey                 = 4,
+                SetConfig                      = 5,
+                GenerateRandomBytes            = 7,
+                ImportLotusKey                 = 9,
+                DecryptLotusMessage            = 10,
+                IsDevelopment                  = 11,
+                GenerateSpecificAesKey         = 12,
+                DecryptRsaPrivateKeyDeprecated = 13,
+                DecryptRsaPrivateKey           = 13,
+                DecryptAesKey                  = 14,
+                CryptAesCtr                    = 15,
+                ComputeCmac                    = 16,
+                ImportEsKey                    = 17,
+                UnwrapTitleKey                 = 18,
+                LoadTitleKey                   = 19,
 
                 /* 2.0.0+ */
-                UnwrapCommonTitleKey        = 20,
-                AllocateAesKeyslot          = 21,
-                FreeAesKeyslot              = 22,
-                GetAesKeyslotAvailableEvent = 23,
+                UnwrapCommonTitleKey           = 20,
+                AllocateAesKeyslot             = 21,
+                FreeAesKeyslot                 = 22,
+                GetAesKeyslotAvailableEvent    = 23,
 
                 /* 3.0.0+ */
-                SetBootReason               = 24,
-                GetBootReason               = 25,
+                SetBootReason                  = 24,
+                GetBootReason                  = 25,
 
                 /* 5.0.0+ */
-                ImportSslKey                = 26,
-                SslExpMod                   = 27,
-                ImportDrmKey                = 28,
-                DrmExpMod                   = 29,
-                ReEncryptRsaPrivateKey      = 30,
-                GetPackage2Hash             = 31,
+                ImportSslKey                   = 26,
+                SslExpMod                      = 27,
+                ImportDrmKey                   = 28,
+                DrmExpMod                      = 29,
+                ReEncryptRsaPrivateKey         = 30,
+                GetPackage2Hash                = 31,
 
                 /* 6.0.0+ */
-                UnwrapElicenseKey           = 31, /* re-used command id :( */
-                LoadElicenseKey             = 32,
+                UnwrapElicenseKey              = 31, /* re-used command id :( */
+                LoadElicenseKey                = 32,
             };
         public:
             GeneralService() { /* ... */ }
