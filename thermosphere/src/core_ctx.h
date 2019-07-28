3769493300d3f00de1867f46a3e073500edd2346
TuxSH <tuxsh@sfr.fr>
2019-07-28 19:57:00 +0200

    thermosphere: add core_ctx.c/h

---
+++ thermosphere/src/core_ctx.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019 Atmosphère-NX
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "utils.h"
+
+typedef struct CoreCtx {
+    u32 coreId;
+} CoreCtx;
+
+extern CoreCtx g_coreCtxInstances[4];
+register CoreCtx *currentCoreCtx asm("x18");
