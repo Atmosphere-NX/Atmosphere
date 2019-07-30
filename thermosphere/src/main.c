a291bddcc1aee884f834b3a9d2cc34159b100e44
TuxSH <1922548+TuxSH@users.noreply.github.com>
2019-07-30 02:16:25 +0200

    thermosphere: enable traps, works around qemu brk bug

--- thermosphere/src/main.c
+++ thermosphere/src/main.c
@@ -2,15 +2,17 @@
 #include "core_ctx.h"
 #include "log.h"
 #include "platform/uart.h"
+#include "traps.h"
 
 int main(void)
 {
+    enableTraps();
+
     if (currentCoreCtx->coreId == 0) {
         uartInit(115200);
         serialLog("Hello from Thermosphere!\n");
         __builtin_trap();
     }
-
     else {
         serialLog("Core %u booted\n", currentCoreCtx->coreId);
     }
