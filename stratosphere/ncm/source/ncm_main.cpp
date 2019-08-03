352b7477897a91e86a0c0fe4b4d2447778b3b28c
Adubbz <Adubbz@users.noreply.github.com>
2019-08-03 22:50:56 +1000

    Bump heap size, fix CleanupAllPlaceHolder

--- stratosphere/ncm/source/ncm_main.cpp
+++ stratosphere/ncm/source/ncm_main.cpp
@@ -28,6 +28,7 @@ extern "C" {
     u32 __nx_applet_type = AppletType_None;
 
     #define INNER_HEAP_SIZE 0x60000
+    #define INNER_HEAP_SIZE 0x100000
     size_t nx_inner_heap_size = INNER_HEAP_SIZE;
     char   nx_inner_heap[INNER_HEAP_SIZE];
     
