cc232ef4f825c3cca3aeff978c072c3fc8236b4e
TuxSH <1922548+TuxSH@users.noreply.github.com>
2019-08-04 18:38:32 +0200

    thermosphere: add spinlock code

---
+++ thermosphere/src/spinlock.s
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Atmosphère-NX
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// From Arm TF
+
+
+/*
+ * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+.global spinlockLock
+.type   spinlockLock, %function
+spinlockLock:
+    mov     w2, #1
+    sevl
+    l1:
+        wfe
+        l2:
+            ldaxr       w1, [x0]
+            cbnz        w1, l1
+            stxr        w1, w2, [x0]
+            cbnz        w1, l2
+    ret
+
+.global spinlockUnlock
+.type   spinlockUnlock, %function
+spinlockUnlock:
+    stlr    wzr, [x0]
+    sev
+    ret
