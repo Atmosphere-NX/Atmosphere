f62174abfddb98574b66c31309ea8dabd1569fcd
Adubbz <Adubbz@users.noreply.github.com>
2019-08-13 23:38:24 +1000

    lr: Cleanup lr_redirection

--- stratosphere/ncm/source/impl/lr_redirection.cpp
+++ stratosphere/ncm/source/impl/lr_redirection.cpp
@@ -18,54 +18,75 @@
 
 namespace sts::lr::impl {
 
+    class LocationRedirection  : public util::IntrusiveListBaseNode<LocationRedirection> {
+        NON_COPYABLE(LocationRedirection);
+        NON_MOVEABLE(LocationRedirection);
+        private:
+            ncm::TitleId title_id;
+            Path path;
+            u32 flags;
+        public:
+            LocationRedirection(ncm::TitleId title_id, const Path& path, u32 flags) :
+                title_id(title_id), path(path), flags(flags) { /* ... */ }
+
+            ncm::TitleId GetTitleId() const {
+                return this->title_id;
+            }
+
+            void GetPath(Path *out) const {
+                *out = this->path;
+            }
+
+            u32 GetFlags() const {
+                return this->flags;
+            }
+
+            void SetFlags(u32 flags) {
+                this->flags = flags;
+            }
+    };
+
     bool LocationRedirector::FindRedirection(Path *out, ncm::TitleId title_id) {
         if (this->redirection_list.empty()) {
             return false;
         }
 
-        for (auto it = this->redirection_list.begin(); it != this->redirection_list.end(); it++) {
-            if (it->title_id == title_id) {
-                *out = it->path;
+        for (const auto &redirection : this->redirection_list) {
+            if (redirection.GetTitleId() == title_id) {
+                redirection.GetPath(out);
                 return true;
             }
         }
-
         return false;
     }
 
     void LocationRedirector::SetRedirection(ncm::TitleId title_id, const Path &path, u32 flags) {
         this->EraseRedirection(title_id);
-        auto redirection = new LocationRedirection(title_id, path, flags);
-        this->redirection_list.push_back(*redirection);
+        this->redirection_list.push_back(*(new LocationRedirection(title_id, path, flags)));
     }
 
     void LocationRedirector::SetRedirectionFlags(ncm::TitleId title_id, u32 flags) {
-        if (!this->redirection_list.empty()) {
-            for (auto it = this->redirection_list.begin(); it != this->redirection_list.end(); it++) {
-                if (it->title_id == title_id) {
-                    it->flags = flags;
-                    break;
-                }
+        for (auto &redirection : this->redirection_list) {
+            if (redirection.GetTitleId() == title_id) {
+                redirection.SetFlags(flags);
+                break;
             }
         }
     }
 
     void LocationRedirector::EraseRedirection(ncm::TitleId title_id) {
-        if (!this->redirection_list.empty()) {
-            for (auto it = this->redirection_list.begin(); it != this->redirection_list.end(); it++) {
-                if (it->title_id == title_id) {
-                    auto old = it;
-                    this->redirection_list.erase(old);
-                    delete &(*old);
-                    break;
-                }
+        for (auto &redirection : this->redirection_list) {
+            if (redirection.GetTitleId() == title_id) {
+                this->redirection_list.erase(this->redirection_list.iterator_to(redirection));
+                delete &redirection;
+                break;
             }
         }
     }
 
     void LocationRedirector::ClearRedirections(u32 flags) {
         for (auto it = this->redirection_list.begin(); it != this->redirection_list.end();) {
-            if ((it->flags & flags) == flags) {
+            if ((it->GetFlags() & flags) == flags) {
                 auto old = it;
                 it = this->redirection_list.erase(it);
                 delete &(*old);
@@ -74,7 +95,5 @@ namespace sts::lr::impl {
             }
         }
     }
-
-
 
 }
