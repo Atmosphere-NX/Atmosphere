59be817bb8369b26496dd759428579de83f0426a
Michael Scire <SciresM@gmail.com>
2020-01-02 17:45:30 -0800

    fix header include

--- libraries/libvapours/include/vapours/literals.hpp
+++ libraries/libvapours/include/vapours/literals.hpp
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
-#include "../defines.hpp"
+#include "defines.hpp"
 
 namespace ams { inline namespace literals {
 
