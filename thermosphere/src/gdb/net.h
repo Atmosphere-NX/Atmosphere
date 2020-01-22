c89ce085a6bec211b7213b95d28d07a1cc1644b6
TuxSH <1922548+TuxSH@users.noreply.github.com>
2020-01-22 21:29:44 +0000

    thermopshère: rewrite some gdb/net functions

--- thermosphere/src/gdb/net.h
+++ thermosphere/src/gdb/net.h
@@ -7,24 +7,24 @@
 
 #pragma once
 
-#include "gdb.h"
+#include "../gdb.h"
 #define _REENT_ONLY
 #include <errno.h>
 
-u8 GDB_ComputeChecksum(const char *packetData, u32 len);
-void GDB_EncodeHex(char *dst, const void *src, u32 len);
-u32 GDB_DecodeHex(void *dst, const char *src, u32 len);
-u32 GDB_EscapeBinaryData(u32 *encodedCount, void *dst, const void *src, u32 len, u32 maxLen);
-u32 GDB_UnescapeBinaryData(void *dst, const void *src, u32 len);
-const char *GDB_ParseIntegerList(u32 *dst, const char *src, u32 nb, char sep, char lastSep, u32 base, bool allowPrefix);
-const char *GDB_ParseHexIntegerList(u32 *dst, const char *src, u32 nb, char lastSep);
-const char *GDB_ParseIntegerList64(u64 *dst, const char *src, u32 nb, char sep, char lastSep, u32 base, bool allowPrefix);
-const char *GDB_ParseHexIntegerList64(u64 *dst, const char *src, u32 nb, char lastSep);
+u8 GDB_ComputeChecksum(const char *packetData, size_t len);
+void GDB_EncodeHex(char *dst, const void *src, size_t len);
+u32 GDB_DecodeHex(void *dst, const char *src, size_t len);
+u32 GDB_EscapeBinaryData(u32 *encodedCount, void *dst, const void *src, size_t len, size_t maxLen);
+u32 GDB_UnescapeBinaryData(void *dst, const void *src, size_t len);
+
+const char *GDB_ParseIntegerList(unsigned long *dst, const char *src, size_t nb, char sep, char lastSep, u32 base, bool allowPrefix);
+const char *GDB_ParseHexIntegerList(unsigned long *dst, const char *src, size_t nb, char lastSep);
+
 int GDB_ReceivePacket(GDBContext *ctx);
-int GDB_SendPacket(GDBContext *ctx, const char *packetData, u32 len);
+int GDB_SendPacket(GDBContext *ctx, const char *packetData, size_t len);
 int GDB_SendFormattedPacket(GDBContext *ctx, const char *packetDataFmt, ...);
-int GDB_SendHexPacket(GDBContext *ctx, const void *packetData, u32 len);
-int GDB_SendStreamData(GDBContext *ctx, const char *streamData, u32 offset, u32 length, u32 totalSize, bool forceEmptyLast);
+int GDB_SendHexPacket(GDBContext *ctx, const void *packetData, size_t len);
+int GDB_SendStreamData(GDBContext *ctx, const char *streamData, size_t offset, size_t length, size_t totalSize, bool forceEmptyLast);
 int GDB_SendDebugString(GDBContext *ctx, const char *fmt, ...); // unsecure
 int GDB_ReplyEmpty(GDBContext *ctx);
 int GDB_ReplyOk(GDBContext *ctx);
