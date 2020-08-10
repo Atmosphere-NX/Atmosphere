b25a01633e30c120d59baa805dd83bce83829470
fincs <fincs@devkitpro.org>
2020-08-10 21:54:13 +0200

    kern: add stubbed KDevicePageTable implementation for systems without iommu

--- libraries/libvapours/include/vapours/svc/board/generic/svc_device_name.hpp
+++ libraries/libvapours/include/vapours/svc/board/generic/svc_device_name.hpp
@@ -2,7 +2,7 @@
  * Copyright (c) 2018-2020 Atmosphère-NX
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
+ * under the terms and conditions of the GNU General Public License
  * version 2, as published by the Free Software Foundation.
  *
  * This program is distributed in the hope it will be useful, but WITHOUT
@@ -13,22 +13,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
-#include <vapours/svc/svc_common.hpp>
+#include <vapours/svc/svc_types_common.hpp>
 
-#if defined(ATMOSPHERE_BOARD_NINTENDO_NX)
+namespace ams::svc::board::generic {
 
-    #include <vapours/svc/board/nintendo/nx/svc_device_name.hpp>
-    namespace ams::svc {
-        using namespace ams::svc::board::nintendo::nx;
-    }
+    enum DeviceName {
+        /* If there is no smmu, there are no device names. */
+        DeviceName_Count = 0,
+    };
 
-#else
-
-    #include <vapours/svc/board/generic/svc_device_name.hpp>
-    namespace ams::svc {
-        using namespace ams::svc::board::generic;
-    }
-
-#endif
+}
