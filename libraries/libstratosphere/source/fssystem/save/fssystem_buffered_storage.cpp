b5f72b9f20e0f4af62a5c62dccdda070736a736e
Michael Scire <SciresM@gmail.com>
2021-01-28 14:28:45 -0800

    fssystem: fix bug in BufferedStorage

--- libraries/libstratosphere/source/fssystem/save/fssystem_buffered_storage.cpp
+++ libraries/libstratosphere/source/fssystem/save/fssystem_buffered_storage.cpp
@@ -899,7 +899,7 @@ namespace ams::fssystem::save {
             const s64 cur_offset_end = offset + *size;
             size_t cur_size = 0;
 
-            if (!util::IsAligned(offset, this->block_size)) {
+            if (!util::IsAligned(cur_offset_end, this->block_size)) {
                 const s64 aligned_size = cur_offset_end - util::AlignDown(cur_offset_end, this->block_size);
                 cur_size = std::min(aligned_size, static_cast<s64>(*size));
             } else if (*size < this->block_size) {
