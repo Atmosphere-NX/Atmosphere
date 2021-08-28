6c5f2804abe51f56c54f5319841dd894297499e1
Michael Scire <SciresM@gmail.com>
2021-08-28 01:15:15 -0700

    fusee_cpp: implement all required key derivation

--- libraries/libexosphere/include/exosphere/pkg1/pkg1_se_key_slots.hpp
+++ libraries/libexosphere/include/exosphere/pkg1/pkg1_se_key_slots.hpp
@@ -41,6 +41,7 @@ namespace ams::pkg1 {
         AesKeySlot_SecmonEnd                      = AesKeySlot_Count,
 
         /* Used only during boot. */
+        AesKeySlot_TsecRootDev                    = 11,
         AesKeySlot_Tsec                           = 12,
         AesKeySlot_TsecRoot                       = 13,
         AesKeySlot_SecureBoot                     = 14,
@@ -53,6 +54,13 @@ namespace ams::pkg1 {
         /* Mariko only keyslots, used during boot. */
         AesKeySlot_MarikoKek                      = 12,
         AesKeySlot_MarikoBek                      = 13,
+
+        /* Bootloader keyslots, for fusee only. */
+        AesKeySlot_BootloaderSystem0              = 2,
+        AesKeySlot_BootloaderSystem1              = 3,
+        AesKeySlot_BootloaderDeviceMaster         = 6,
+        AesKeySlot_BootloaderMaster               = 7,
+        AesKeySlot_BootloaderTemporary            = 8,
     };
 
     enum RsaKeySlot {
