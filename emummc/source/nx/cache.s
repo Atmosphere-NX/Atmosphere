4f763b2aa401ac3e3d699ec8c866ae9b3c8fb28d
Michael Scire <SciresM@gmail.com>
2022-03-23 09:22:22 -0700

    emummc: integrate updates for 14.0.0

--- emummc/source/nx/cache.s
+++ emummc/source/nx/cache.s
@@ -27,6 +27,10 @@ CODE_BEGIN armDCacheFlush
 	bic x8, x0, x10
 	mov x10, x1
 
+	mov w1, #1
+	mrs x0, tpidrro_el0
+	strb w1, [x0, #0x104]
+
 armDCacheFlush_L0:
 	dc  civac, x8
 	add x8, x8, x9
@@ -34,6 +38,9 @@ armDCacheFlush_L0:
 	bcc armDCacheFlush_L0
 
 	dsb sy
+
+	strb wzr, [x0, #0x104]
+
 	ret
 CODE_END
 
@@ -48,6 +55,10 @@ CODE_BEGIN armDCacheClean
 	bic x8, x0, x10
 	mov x10, x1
 
+	mov w1, #1
+	mrs x0, tpidrro_el0
+	strb w1, [x0, #0x104]
+
 armDCacheClean_L0:
 	dc  cvac, x8
 	add x8, x8, x9
@@ -55,6 +66,9 @@ armDCacheClean_L0:
 	bcc armDCacheClean_L0
 
 	dsb sy
+
+	strb wzr, [x0, #0x104]
+
 	ret
 CODE_END
 
@@ -68,6 +82,10 @@ CODE_BEGIN armICacheInvalidate
 	bic x8, x0, x10
 	mov x10, x1
 
+	mov w1, #1
+	mrs x0, tpidrro_el0
+	strb w1, [x0, #0x104]
+
 armICacheInvalidate_L0:
 	ic  ivau, x8
 	add x8, x8, x9
@@ -75,6 +93,9 @@ armICacheInvalidate_L0:
 	bcc armICacheInvalidate_L0
 
 	dsb sy
+
+	strb wzr, [x0, #0x104]
+
 	ret
 CODE_END
 
@@ -89,6 +110,10 @@ CODE_BEGIN armDCacheZero
 	bic x8, x0, x10
 	mov x10, x1
 
+	mov w1, #1
+	mrs x0, tpidrro_el0
+	strb w1, [x0, #0x104]
+
 armDCacheZero_L0:
 	dc  zva, x8
 	add x8, x8, x9
@@ -96,5 +121,8 @@ armDCacheZero_L0:
 	bcc armDCacheZero_L0
 
 	dsb sy
+
+	strb wzr, [x0, #0x104]
+
 	ret
 CODE_END
