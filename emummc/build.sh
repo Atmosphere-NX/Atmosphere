e3cb5e74b98b704afdee3762dd13f92154a89abc
Michael Scire <SciresM@gmail.com>
2022-03-23 09:20:31 -0700

    emummc: temp delete

--- emummc/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-make clean
-make -j
-./hactool.exe -t kip emummc.kip --uncompressed emummc_unpacked.kip
-python2.7 tools/kip1converter.py emummc_unpacked.kip emummc.data
-cat emummc.caps emummc.data > emummc.kipm
