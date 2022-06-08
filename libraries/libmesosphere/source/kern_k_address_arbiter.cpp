20d200471d31af13c192c4fc46a0bc3238c9392d
Michael Scire <SciresM@gmail.com>
2022-06-07 23:49:31 -0700

    kern: optimize postfix-compare to prefix-compare, result logic is identical but breaks earlier in some paths

--- libraries/libmesosphere/source/kern_k_address_arbiter.cpp
+++ libraries/libmesosphere/source/kern_k_address_arbiter.cpp
@@ -132,7 +132,7 @@ namespace ams::kern {
                     auto tmp_it = it;
                     s32 tmp_num_waiters = 0;
                     while ((++tmp_it != m_tree.end()) && (tmp_it->GetAddressArbiterKey() == addr)) {
-                        if ((tmp_num_waiters++) >= count) {
+                        if ((++tmp_num_waiters) >= count) {
                             break;
                         }
                     }
