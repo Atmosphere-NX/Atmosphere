8208027097a43c9c96d4d2b94d6247c7a44e2654
rashevskyv <rashevsky.v@gmail.com>
2024-06-04 16:51:06 +0300

    KEFIR: Changed bootscreen

--- stratosphere/boot/source/boot_splash_screen.cpp
+++ stratosphere/boot/source/boot_splash_screen.cpp
@@ -24,7 +24,7 @@ namespace ams::boot {
 
 /* Include splash screen into anonymous namespace. */
 /* TODO: Compile-time switch for splash_screen_text.hpp? */
-#include "boot_splash_screen_notext.inc"
+#include "boot_splash_kefir.inc"
 
     }
 
@@ -38,7 +38,7 @@ namespace ams::boot {
         {
             /* Splash screen is shown for 2 seconds. */
             ShowDisplay(SplashScreenX, SplashScreenY, SplashScreenW, SplashScreenH, SplashScreen);
-            os::SleepThread(TimeSpan::FromSeconds(2));
+            os::SleepThread(TimeSpan::FromSeconds(4));
         }
         FinalizeDisplay();
     }
