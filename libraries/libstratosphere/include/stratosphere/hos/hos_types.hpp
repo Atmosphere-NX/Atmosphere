59ecd7aeb3e604cd25589a946c5b9069b35321b7
Michael Scire <SciresM@gmail.com>
2024-06-10 22:17:23 -0700

    ams: bump to 1.7.1 (support for 18.1.0)

--- libraries/libstratosphere/include/stratosphere/hos/hos_types.hpp
+++ libraries/libstratosphere/include/stratosphere/hos/hos_types.hpp
@@ -83,6 +83,7 @@ namespace ams::hos {
         Version_17_0_0  = ::ams::TargetFirmware_17_0_0,
         Version_17_0_1  = ::ams::TargetFirmware_17_0_1,
         Version_18_0_0  = ::ams::TargetFirmware_18_0_0,
+        Version_18_1_0  = ::ams::TargetFirmware_18_1_0,
 
         Version_Current = ::ams::TargetFirmware_Current,
 
