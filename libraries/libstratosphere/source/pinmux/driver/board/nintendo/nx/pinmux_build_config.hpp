5b0a4830d4b7472fbb6d5b17990bfbe0473f95ff
Alula <git@alula.me>
2026-03-17 12:16:23 +0100

    pinmux: add a build time option to configure JC rail uart

---
+++ libraries/libstratosphere/source/pinmux/driver/board/nintendo/nx/pinmux_build_config.hpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Atmosphère-NX
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <stratosphere.hpp>
+
+// #define AMS_PINMUX_CONFIG_RIGHT_RAIL_AS_UART
+// #define AMS_PINMUX_CONFIG_LEFT_RAIL_AS_UART
