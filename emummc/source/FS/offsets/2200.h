5dfdc6e8b0a52281082328b25870d2367432253b
hexkyz <mike.hexkyz@gmail.com>
2026-03-20 00:17:19 +0000

    Merge emummc changes from pull request #2743
    Remaining changes are duplicated in pull request #2744

---
+++ emummc/source/FS/offsets/2200.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 m4xw <m4x@m4xw.net>
+ * Copyright (c) 2019 Atmosphere-NX
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __FS_2200_H__
+#define __FS_2200_H__
+
+// Accessor vtable getters
+#define FS_OFFSET_2200_SDMMC_ACCESSOR_GC   0x1B01C0
+#define FS_OFFSET_2200_SDMMC_ACCESSOR_SD   0x1B21C0
+#define FS_OFFSET_2200_SDMMC_ACCESSOR_NAND 0x1B07F0
+
+// Hooks
+#define FS_OFFSET_2200_SDMMC_WRAPPER_READ  0x1AC0F0
+#define FS_OFFSET_2200_SDMMC_WRAPPER_WRITE 0x1AC150
+#define FS_OFFSET_2200_RTLD                0x2DED0
+#define FS_OFFSET_2200_RTLD_DESTINATION    ((uintptr_t)(INT64_C(-0x4C)))
+
+#define FS_OFFSET_2200_CLKRST_SET_MIN_V_CLK_RATE 0x1CF260
+
+// Misc funcs
+#define FS_OFFSET_2200_LOCK_MUTEX          0x1CF260
+#define FS_OFFSET_2200_UNLOCK_MUTEX        0x1A4F40
+
+#define FS_OFFSET_2200_SDMMC_WRAPPER_CONTROLLER_OPEN  0x1AC0B0
+#define FS_OFFSET_2200_SDMMC_WRAPPER_CONTROLLER_CLOSE 0x1AC0D0
+
+// Misc Data
+#define FS_OFFSET_2200_SD_MUTEX            0xFF1408
+#define FS_OFFSET_2200_NAND_MUTEX          0xFECD00
+#define FS_OFFSET_2200_ACTIVE_PARTITION    0xFECD40
+#define FS_OFFSET_2200_SDMMC_DAS_HANDLE    0xFCEB98
+
+// NOPs
+#define FS_OFFSET_2200_SD_DAS_INIT         0x2B438
+
+// Nintendo Paths
+#define FS_OFFSET_2200_NINTENDO_PATHS \
+{ \
+    {.opcode_reg = 3, .adrp_offset = 0x000715EC, .add_rel_offset = 0x00000004}, \
+    {.opcode_reg = 3, .adrp_offset = 0x00082E64, .add_rel_offset = 0x00000004}, \
+    {.opcode_reg = 4, .adrp_offset = 0x0008B888, .add_rel_offset = 0x00000004}, \
+    {.opcode_reg = 4, .adrp_offset = 0x000A1B1C, .add_rel_offset = 0x00000004}, \
+    {.opcode_reg = 0, .adrp_offset = 0, .add_rel_offset = 0}, \
+}
+
+#endif // __FS_2200_H__
